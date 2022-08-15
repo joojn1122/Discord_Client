2b0ba0303eb4784988b56f32eb3ef72285dcb081
joojn <85359001+joojn1122@users.noreply.github.com>
2022-08-15 20:20:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
 from discord_client import Client
+import random
 
-#from karelbot import get_response
-from ai import get_response
-
-from word_football import get_random_word
-
-
-with open("token.txt", "r") as f:
-	token = f.read()
-
-client = Client(token)
-client.start_gateway()
+client = Client("<YOUR TOKEN">)
+client.start_gateway() # connect to discord server to reveive events (without this events won't work)
 
 def on_message(event):
 	username = event["author"]["username"]
-	disc = event["author"]["discriminator"]
 	channel_id = event["channel_id"]
-
-	# if == return
-
 	message = event["content"]
 
-	if "<" in message and ">" in message: return
-
 	print(f"{username} said {message}")
-
+	
+	# send typing action
 	client.send_typing_action(channel_id)
-
-	if channel_id == "1007582760380137614":
-		response = get_random_word(message[::-1][0])
-
-		if response == None:
-			return
-
-		time.sleep(len(response) / 5)
-
-	else:
-		# sleep implemented in get_response
-		response = get_response(message)
-
-	client.send_chat_message(channel_id, response, reply_msg_id=(event["id"] if random.randint(0, 1) == 0 else None))
+	
+	# send welcome message and there's 50% change to reply to the user's message or send it normally
+	client.send_chat_message(channel_id, f"Hello {username}!", reply_msg_id=(event["id"] if random.randint(0, 1) == 0 else None))
 
 
 client.hook_event("MESSAGE_CREATE", on_message)
