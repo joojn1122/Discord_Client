1c3e42c7b0ac1dd7795760d069144c5e8c6dc80b
joojn <85359001+joojn1122@users.noreply.github.com>
2022-11-26 14:14:37 +0100

    Update and rename main.py to example.py

--- example.py
+++ example.py
@@ -4,9 +4,10 @@ import random
 client = Client("<YOUR TOKEN">)
 client.start_gateway() # connect to discord server to reveive events (without this events won't work)
 
-profile = client.action.get_profile()
+# get client's profile (in dict)
+profile: dict = client.action.get_profile()
 
-def on_message(event):
+def on_message(event: dict) -> None:
 	username = event["author"]["username"]
 	disc = event["author"]["discriminator"]
 	channel_id = event["channel_id"]
@@ -24,5 +25,5 @@ def on_message(event):
 	# send welcome message and there's 50% change to reply to the user's message or send it normally
 	client.action.send_chat_message(channel_id, f"Hello {username}!", reply_msg_id=(event["id"] if random.randint(0, 1) == 0 else None))
 
-
+# you can hook events, you can find events here https://discord.com/developers/docs/topics/gateway
 client.hook_event("MESSAGE_CREATE", on_message)
